3de4ba063aec8eebfa208d47affc7a1a7dadda49
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-19 12:12:46 +0600

    formate string done

---
+++ formate_string_01.py
@@ -0,0 +1,17 @@
+age = 25
+
+name = "Rokib"
+
+name = f"My name is {name}. I am {age} years old."
+
+print(name)
+
+price = 49
+
+txt = f"The price is {price:.2f} dollars"
+
+print(txt)
+
+txt2 = f"The price is {20 * price:.2f} dollars"
+
+print(txt2)
