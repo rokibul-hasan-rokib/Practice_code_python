4639161239f58da6d73ae4fe84a207832e263e55
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-19 12:45:57 +0600

    change list item

---
+++ change_list_item.py
@@ -0,0 +1,7 @@
+list = ['apple', 'banana', 'cherry']
+list[1] = "blackcurrant"
+print(list)
+
+list_two = ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon', 'mango']
+list_two[1:3] = ["blackcurrant", "watermelon"]
+print(list_two)
