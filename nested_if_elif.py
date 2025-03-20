0c04d7e55f091740657bcc215f027ec36e3d92df
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-20 11:58:43 +0600

    nested if else

---
+++ nested_if_elif.py
@@ -0,0 +1,19 @@
+a = 40
+b = 50
+
+if a > b:
+    print("a is greater than b")
+    if a > 100:
+        print("a is greater than 100")
+    elif a > 50:
+        print("a is greater than 50")
+    else:
+        print("a is less than 50")
+else:
+    print("b is greater than a")
+    if b > 100:
+        print("b is greater than 100")
+    elif b > 50:
+        print("b is greater than 50")
+    else:
+        print("b is less than 50")
