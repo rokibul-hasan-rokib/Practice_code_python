1e3ad5da9bcef22d2cd959e840a95d3f84e7d9f2
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-20 12:30:36 +0600

    function argument pass

---
+++ function_001.py
@@ -0,0 +1,17 @@
+def myFunction():
+    print("Hello I am Rokib")
+    
+myFunction();
+
+def myFunction(fname):
+    print(fname + " is my Name")
+    
+myFunction("Emil");
+myFunction("Tobias");
+myFunction("Linus");    
+
+def myFunction(fname, lname):
+    print(fname + " " + lname)
+    
+myFunction("Emil", "Refsnes")
+
