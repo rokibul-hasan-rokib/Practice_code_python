624bb7697599c6abe15bc9230f5ee6feff15962e
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-20 12:20:38 +0600

    while loop with breaks and continue function

---
+++ while_loop_01.py
@@ -0,0 +1,21 @@
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+    
+    
+i = 0
+while i < 6:
+    print(i)
+    i += 1
+    if i == 3:
+        break
+    
+
+        
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
