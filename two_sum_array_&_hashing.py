62f7e14649b7ce0b2569850d527da2c06f62c057
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-03-26 11:27:02 +0600

    two some a rray of hashing

---
+++ two_sum_array_&_hashing.py
@@ -0,0 +1,10 @@
+def two_sum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
+    return []
+
+print(two_sum([2, 7, 11, 15], 9))
